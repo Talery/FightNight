27595939ba7a234d4affa38801041a5c4d0ec736
Talery <63315043+Talery@users.noreply.github.com>
2026-08-13 21:34:19 +0300

    style: load typography overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './design-tests.css'
+import './design-typography.css'
 import ConceptGallery from './concepts/ConceptGallery'
 
 const showConcepts = new URLSearchParams(window.location.search).has('concepts')
