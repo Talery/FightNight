16b41d922d64398099240d28d6e839ad00e0610b
Talery <63315043+Talery@users.noreply.github.com>
2026-08-13 18:22:23 +0300

    style: enable design test layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './design-tests.css'
 import ConceptGallery from './concepts/ConceptGallery'
 
 const showConcepts = new URLSearchParams(window.location.search).has('concepts')
