488a23f2e3985759b3bdab0d9899cbbf583b64a9
Talery <sergeytalery@gmail.com>
2026-08-14 20:32:11 +0300

    revert: restore stable pre-v11 design

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './design-tests.css'
-import './design-frames-v11.css'
-import './design-type-v11.css'
-import './design-layout-v11.css'
+import './design-typography.css'
+import './design-frames-v3.css'
 import ConceptGallery from './concepts/ConceptGallery'
 
 const showConcepts = new URLSearchParams(window.location.search).has('concepts')
